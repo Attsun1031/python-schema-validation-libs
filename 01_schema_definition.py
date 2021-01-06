cc59503078d7204f473b97c75d791527e25ad9f9
Attsun1031 <atsumi.tatsuya@gmail.com>
2021-01-06 22:30:05 +0900

    remove jsonschema

--- 01_schema_definition.py
+++ 01_schema_definition.py
@@ -1,23 +1,87 @@
 """
 スキーマ定義に関する比較
 """
+
 import typing
 import pydantic as pd
+import marshmallow as ma
+import attr, cattr
+import jsonschema
+import cerberus
 
+
+# テスト対象データ
+data = {"i": "1", "j": None}
 
 ################################################################################
-# Pydantic
+# pydantic
 ################################################################################
-print("=== Pydantic")
+print("=== pydantic")
 
 
-class PydanticModle1(pd.BaseModel):
+class PydanticModel(pd.BaseModel):
     # Required
     i: int
     # Nullable & Required
     j: typing.Optional[int] = pd.Field(...)
     # Optional
-    k: int = 0
+    k: int = 3
 
-print(PydanticModle1(i=1, j=None))
+print(PydanticModel(**data))
+
+
+################################################################################
+# marshmallow
+################################################################################
+print("=== marshmallow")
+
+
+class MarshmallowModel(ma.Schema):
+    # Required
+    i = ma.fields.Int(required=True, allow_none=False)
+    # Nullable & Required
+    j = ma.fields.Int(required=True, allow_none=True)
+    # Optional
+    k = ma.fields.Int(missing=3)
+
+
+print(MarshmallowModel().load(data))
+
+
+################################################################################
+# attr
+################################################################################
+print("=== attr")
+
+
+@attr.s
+class AttrsModel:
+    # Required
+    i = attr.ib(validator=attr.validators.instance_of(int), converter=int)
+    # Nullable & Required
+    j = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(int)), converter=lambda i: int(i) if i is not None else None)
+    # Optional
+    k = attr.ib(default=3, validator=attr.validators.instance_of(int), converter=int)
+
+
+print(cattr.structure(data, AttrsModel))
+
+
+
+################################################################################
+# cerberus
+################################################################################
+print("=== cerberus")
+
+
+cerberus_schema = {
+    # Required
+    'i': {'type': 'integer', 'required': True, 'coerce': int},
+    # Nullable & Required
+    'j': {'type': 'integer', 'required': True, 'nullable': True, 'coerce': int},
+    # Optional
+    'k': {'type': 'integer', 'default': 3, 'coerce': int},
+}
+cerberus_validator = cerberus.Validator(cerberus_schema)
+print(cerberus_validator.validated(data))
 
